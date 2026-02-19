d5d6d4a4057249ceb293126c2549d246e337a71b
a8anassis <a8anassis@gmail.com>
2026-02-19 19:04:58 +0200

    feat: add json examples

--- objects/json.js
+++ objects/json.js
@@ -1,9 +1,9 @@
 const jsObj = { id: 1, title: "Athens", population: 3000000 }
 
-// convert to JSON string
+// convert to JSON string -- serialization
 const jsonString = JSON.stringify(jsObj);
 console.log(jsonString)  // {"id":1,"title":"Athens","population":3000000}     
 
-// convert back to JavaScript object
+// convert back to JavaScript object -- deserialization
 const jsObj2 = JSON.parse(jsonString);
 console.log(jsObj2) // { id: 1, title: 'Athens', population: 3000000 }
