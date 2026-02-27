a6f1180121b0b90b53c829bead2bfcd5771aaeab
a8anassis <a8anassis@gmail.com>
2026-02-27 19:24:43 +0200

    feat: add logic for state management and also controllers and model based on MVC pattern

---
+++ apps/counter-app/main.js
@@ -0,0 +1,51 @@
+const DEFAULT = 0
+let counter = DEFAULT
+
+window.addEventListener('DOMContentLoaded', function() {
+
+    document.querySelector('#btnDecr').addEventListener('click', () => onDecreasedClicked())
+    document.querySelector('#btnReset').addEventListener('click', () => onResetClicked())
+    document.querySelector('#btnIncr').addEventListener('click', () => onIncreasedClicked())
+})
+
+// Controllers
+function onDecreasedClicked() {
+    // Validation
+    decreaseCounter()
+}
+
+function onResetClicked() {
+    // Validation
+    resetCounter()
+}
+
+function onIncreasedClicked() {
+    // Validation
+    increaseCounter()
+}
+
+// Model
+function decreaseCounter() {
+    counter--
+}
+
+function resetCounter() {
+    counter = DEFAULT
+}
+
+function increaseCounter() {
+    counter++
+}
+
+// View
+function render() {
+    const counterEl = document.querySelector('#counter')
+    counterEl.textContent = counter
+    styleCounter(counterEl)
+}
+
+function styleCounter(counterEl) {
+    counterEl.classList.remove('color-green', 'color-red', 'color-black')
+    counterEl.add(counter > 0 ? 'color-green' : counter < 0 ? 'color-red' : 'color-black')
+}
+
